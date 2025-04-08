00b8da665e0c54ff0cea79fd746b50bfff179f27
Nishanth <nishanth1596@gmail.com>
2025-04-08 20:36:28 +0530

    feat: improved responsiveness for aboutUsSection for the tablet screen, yet to improve for desktop

--- src/feature/about/AboutUsSection.tsx
+++ src/feature/about/AboutUsSection.tsx
@@ -1,12 +1,27 @@
+// Best Gear Section Images
 import bestGearMobileImg from "/assets/shared/mobile/image-best-gear.jpg";
+import bestGearTabletImg from "/assets/shared/tablet/image-best-gear.jpg";
+import bestGearDesktopImg from "/assets/shared/desktop/image-best-gear.jpg";
 
 function AboutUsSection() {
   return (
-    <section className="mx-6 mt-[7.5rem]">
-      <img src={bestGearMobileImg} alt="" />
+    <section className="mx-6 mt-[7.5rem] sm:mx-10 sm:mt-24">
+      <picture>
+        <source srcSet={bestGearMobileImg} media="(max-width:767px)" />
+        <source
+          srcSet={bestGearTabletImg}
+          media="(min-width:767px) and (max-width:1023px)"
+        />
+        <source srcSet={bestGearDesktopImg} media="(min-width:1024px)" />
+        <img
+          src={bestGearMobileImg}
+          alt="A man listening to music in headphones"
+          className="rounded-lg"
+        />
+      </picture>
 
-      <article className="mt-10 text-center">
-        <h3 className="text-[1.75rem] leading-[2.34rem] font-bold tracking-[1px] uppercase">
+      <article className="mt-10 text-center sm:mx-auto sm:mt-16 sm:max-w-[35.81rem]">
+        <h3 className="text-[1.75rem] leading-[2.34rem] font-bold tracking-[1px] uppercase sm:text-[2.5rem] sm:leading-11">
           Bringing you the <span className="text-PrimaryColor">best</span> audio
           gear
         </h3>
