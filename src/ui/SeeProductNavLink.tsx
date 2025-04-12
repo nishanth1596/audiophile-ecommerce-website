ba161e741cbb88cf32faae04cc49d6321221cf39
Nishanth <nishanth1596@gmail.com>
2025-04-12 21:24:12 +0530

    refactor: refactored the NavLink for applying hover transisitons

--- src/ui/SeeProductNavLink.tsx
+++ src/ui/SeeProductNavLink.tsx
@@ -2,24 +2,29 @@ import { Link } from "react-router-dom";
 
 type SeeProductNavLinkProps = {
   navigateTo: string;
-  backgroundColor: string;
-  textColor: string;
+  backgroundColor: "black" | "orange" | "lightGray";
   isBorderRequired: boolean;
 };
 
 function SeeProductNavLink({
   navigateTo,
   backgroundColor,
-  textColor,
   isBorderRequired,
 }: SeeProductNavLinkProps) {
+  const colorClassMap = {
+    orange: "bg-PrimaryColor text-White hover:bg-SecondaryColor",
+    black:
+      "bg-PureBlack text-White hover:bg-LightGray hover:text-PureBlack hover:outline-2",
+    lightGray: "bg-LightGray hover:bg-PureBlack hover:text-White",
+  };
+
+  const colorClass = colorClassMap[backgroundColor];
+
   return (
     <Link
       to={navigateTo}
-      className="inline-block px-8 py-4 text-[0.813rem] leading-[1.13rem] font-bold tracking-[1px] uppercase lg:self-start"
+      className={`${colorClass} relative z-30 inline-block cursor-pointer px-8 py-4 text-[0.813rem] leading-[1.13rem] font-bold tracking-[1px] uppercase transition-all duration-300 ease-in-out lg:self-start`}
       style={{
-        backgroundColor: backgroundColor,
-        color: textColor,
         boxShadow: isBorderRequired ? "0 0 0 2px #000" : "",
       }}
     >
